5465b1dce3f563b5910c602cb1ffddc1d2008330
saggita1 <69651233+saggita1@users.noreply.github.com>
2021-02-07 23:26:42 -0400

    inicio de carregar palavras

--- principal.py
+++ principal.py
@@ -1,11 +1,29 @@
 from tkinter import *
+import sqlite3
 from config_database import inserir, deletar
 
 root = Tk()
 root.geometry("500x300")
 root.title("FlashCards")
 
-# funções -printa o entry
+# variáveis globais
+tu_at = 0 # Tupla Atual
+p_ing = 0 # palavra em inglês
+p_pt = 0 # palavra em português
+
+# funções
+def carregar_palavras():
+    global lista_palavras
+
+    # sqlite config
+    banco = sqlite3.connect('database_palavras.db')
+    cursor = banco.cursor()
+    
+    cursor.execute("SELECT * FROM pessoas")
+    lista_palavras = cursor.fetchall()
+    banco.close()
+
+
 def confirme():
     #novo = e.get()
     print(e.get().split())
@@ -52,14 +70,16 @@ def nova_janela():
     botao = Button(top, text="CONFIRMAR", command=validar).place(x=160, y=70)
 
 
+# carregando as palavras
+carregar_palavras()
 # label configs
-label = Label(root, text="\n\nRYAN", font=("Times", 22))
+label = Label(root, text=f"\n\n{lista_palavras[tu_at][p_ing]}", font=("Times", 22))
 label.pack()
 
 # botões, SÓ PODE USAR O PLACE 
 botao1 = Button(root, text="ANTERIOR", font=("Times", 16), bd=4)
 botao2 = Button(root, text="PRÓXIMA", font=("Times", 16), bd=4)
-confirmar = Button(root, text="CONFIRMAR", font=("Times", 7), activebackground="green", command=confirme)
+confirmar = Button(root, text="CONFIRMAR", font=("Times", 7), activebackground="green", command=carregar_palavras)
 confg_data = Button(root, text="DATABASE", font=('Times', 16), bd=4, command=nova_janela)
 
 
